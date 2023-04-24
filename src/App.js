16616d3733386f300bb6cb41289838d8060f8bf9
Pragnesh Shrimal <iampragnesh05>
2023-04-24 14:04:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import {useState, useEffect} from 'react'
+
+
+const App = () => {
+  const [ value, setValue] = useState(null)
+  const [ message, setMessage] = useState(null)
+  const [previousChats, setPreviousChats] = useState([])
+  const [currentTitle, setCurrentTitle] = useState(null)
+  const createNewChat = () => {
+    setMessage(null) 
+    setValue("")
+    setCurrentTitle(null)
+  }
+
+const handleClick = (uniqueTitle) => {
+  setCurrentTitle(uniqueTitle)
+  setMessage(null) 
+  setValue("")
+}
+
+
+  const getMessages = async() => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: value
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+    try {
+      const response = await fetch('http://localhost:8000/completions', options)
+      const data = await response.json()
+      setMessage(data.choices[0].message)
+    }
+    catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    console.log(currentTitle, value, message)
+    if(!currentTitle && value && message) {
+      setCurrentTitle(value)
+    }
+    if(currentTitle && value && message) {
+      setPreviousChats(previousChats => (
+        [...previousChats, 
+        {
+            title: currentTitle,
+            role: "user",
+            content: value
+        }, 
+        {
+          title: currentTitle,
+          role: message.role,
+          content: message.content
+        }
+      ]
+      ))
+    }
+  }, [message, currentTitle])
+  
+  console.log(previousChats)
+
+  const currentChat = previousChats.filter(previousChats => previousChats.title === currentTitle)
+  const uniqueTitles = Array.from(new Set(previousChats.map(previousChats => previousChats.title)))
+  console.log(uniqueTitles)
+  return (
+    <div className="app">
+      <section className="side-bar">
+      <button onClick={createNewChat}>+ New chat</button>
+      <ul className="history">
+        {uniqueTitles.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+      </ul>
+      <nav>
+        Made by Technical Team
+      </nav>
+      </section>
+      <section className="main">
+        {!currentTitle && <h1>ConverseAI</h1>}
+        <ul className="feed">
+          {currentChat.map((chatMessage, index) => <li key={index}>
+            <p className='role'>{chatMessage.role}</p>
+            <p>{chatMessage.content}</p>
+          </li>)}
+        </ul>
+
+        <div className="bottom-section">
+          <div className="input-container">
+            <input value={value} onChange={(e) => setValue(e.target.value)}/>
+            <div id="submit" onClick={getMessages}>➢</div>
+          </div>
+          <p className="info"> 
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
